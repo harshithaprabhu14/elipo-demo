70bef582c2aeba6db897823339d335e31d659412
sneha <sneha.meni@peolsolutions.com>
2021-10-19 11:33:47 +0530

    module

--- src/app/layouts/landing.component.ts
+++ src/app/layouts/landing.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./landing.component.css']
 })
 export class LandingComponent implements OnInit {
+  isMobile:boolean=false;
 
   constructor() { }
 
